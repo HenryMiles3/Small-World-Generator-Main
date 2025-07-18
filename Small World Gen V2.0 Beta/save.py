65a62bce0f89e506deb6bb34295092afeec90bdd
HenryMiles3 <henry6miles@gmail.com>
2025-07-18 23:40:30 +0100

    Changed the txt save format to csv.

--- Small World Gen V2.0 Beta/save.py
+++ Small World Gen V2.0 Beta/save.py
@@ -1,5 +1,5 @@
 # Saving to file goes in here.
 
 def start(data):
-    with open('saves.txt','r') as saves:
+    with open('Small World Gen V2.0 Beta\saves.csv','r') as saves:
         data = saves.read()
